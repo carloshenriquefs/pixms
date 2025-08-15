bc354e624718e5c6f5a133d648772b41e2fda4ab
Carlos Henrique <ichfs9108@gmail.com>
2025-08-15 19:30:20 -0300

    created project pixms

---
+++ pixms/src/main/java/tech/buildrun/pixms/PixmsApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.pixms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixmsApplication.class, args);
+	}
+
+}
