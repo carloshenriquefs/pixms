4e02a81c0197dd80dafa13145adeead64f14db7c
Carlos Henrique <ichfs9108@gmail.com>
2025-08-15 20:42:18 -0300

    created other projects

---
+++ pfms/src/test/java/tech/buildrun/pixms/PixmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.pixms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ pjms/src/main/java/tech/buildrun/pjms/PjmsApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.pjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjmsApplication.class, args);
+	}
+
+}
